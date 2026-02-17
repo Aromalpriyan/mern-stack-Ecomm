106aca08d023618bc235a32bdac0b2eaac9fba8a
Aromalpriyan <aromalpriyan213@gmail.com>
2026-02-17 15:24:42 +0530

    dotenv config

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import app from "./src/app.js"
 import config from "./src/config/config.js";
-
+import colors from "colors"
 
 const PORT = config.PORT
 
 
 app.listen(PORT,()=>{
-    console.log(`App is Successfully running at PORT: ${PORT}`);
+    console.log(`App is Successfully running at PORT: ${PORT}`.bgBlack.blue);
 })
